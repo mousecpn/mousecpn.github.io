c8bbc3b7b5073175fd492b2d19a032ca22985976
mousecpn <46233799+mousecpn@users.noreply.github.com>
2026-03-01 14:34:11 +0000

    Deploying to gh-pages from @ mousecpn/mousecpn.github.io@28c04e314c85e6c0f76b530ebe7fce0f2cd81442 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mousecpn/pinhao.song.profile",
+    "data-repo": "mousecpn/mousecpn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
